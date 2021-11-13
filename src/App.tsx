107048109940a225789f4a664a6ac619e2ea6e47
Marcelo Valdivia <72950694@ucsm.edu.pe>
2021-11-12 20:46:10 -0500

    refactor: restructure to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Home } from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
